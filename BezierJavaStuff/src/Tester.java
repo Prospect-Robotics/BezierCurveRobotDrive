59f31e6b2f42dacdff664a002530c15da4708de0
adrianmgg <2adrian2@gmail.com>
2018-07-28 21:33:31 -0700

    added simple test

--- BezierJavaStuff/src/Tester.java
+++ BezierJavaStuff/src/Tester.java
@@ -6,6 +6,9 @@ import javax.swing.JDialog;
 
 public class Tester {
 	public static void main(String[] args) {
-		
+		Bezier bez = new Bezier(new Point(0.0,0.0),new Point(0.0,1.0),new Point(1.0,0.0),new Point(1.0,1.0));
+		for(double t=0.0;t<=1;t+=0.05) {
+			System.out.println(bez.getPoint(t));
+		}
 	}
 }
